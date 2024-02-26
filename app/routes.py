fe6d190add40e7b7c5cfca9f21be379071f73a85
zaikoticale <alejandromatssuno18@gmail.com>
2024-02-26 06:28:31 -0500

    :construction: dockerizando

--- app/routes.py
+++ app/routes.py
@@ -33,4 +33,4 @@ def consult_invoice_information():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0' ,debug=True, port=5000)
